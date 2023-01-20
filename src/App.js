3ce537624e9c5eef79b620bbd0f0048b1a754298
Wonski312 <smarciszedu@gmail.com>
2023-01-20 04:46:16 +0000

    changes to layout

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import SetsPoints from './components/Main/SetsPoints/SetsPoints';
 import WinnerModal from './components/UI/WinnerModal';
 
 import { useSelector, useDispatch } from 'react-redux';
+import classes from'./components/UI/Wrapper.module.css'
 
 function App() {
 
   const modalVisible  = useSelector(state =>state.counter.modalVisible);
   const addInfo = useSelector(state => state.counter.addinfo)
   console.log(addInfo);
-  return <Wrapper className="mainPage">
+  return <Wrapper className={classes.mainPage}>
     <SetsPoints />
     <Main></Main>
     {modalVisible && <WinnerModal player={addInfo.player}></WinnerModal>}
